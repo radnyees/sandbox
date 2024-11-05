c4203e5fcc90879302cd8ee0df55c1a58e8ded3c
radnyees <sarvadnyar21@gmail.com>
2024-11-05 17:24:05 +0000

    EEG Plot

---
+++ missingdatatest.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 1, 3, 7]
+
+# Create the plot
+plt.plot(x, y)
+
+# Add labels and title
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Simple Line Plot')
+
+# Display the plot
+plt.show()
+
