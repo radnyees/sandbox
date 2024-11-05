2229a6743200886c715b702e3c6d4f52d2925269
radnyees <sarvadnyar21@gmail.com>
2024-11-05 16:39:01 +0000

    EEG Signals

--- playground.py
+++ playground.py
@@ -10,7 +10,7 @@ np.random.seed(0)
 data=np.random.randn(1000,4)
 
 #Creating a data frame
-df= pd.DataFrame(data, columns=['Fp1','Fp2','Fp3','Fp4'], index=time)
+df= pd.DataFrame(data, columns=['Fp1','Fp2','F3','F4'], index=time)
 
 # Inspecting first few rows
 print(df.head())
@@ -18,3 +18,41 @@ print(df.head())
 # For checking data types and summary statistics
 print(df.info())
 print(df.describe())
+
+def handling_missing_values():
+    import pandas as pd
+    import numpy as np
+    df_missing = data.copy()
+    df_missing.iloc[::50] = np.nan
+
+    # Identifying missing values
+    print("Number of missing values per column:")
+    print(df_missing.isnull().sum())
+
+    df_dropped = df_missing.dropna()
+    df_interpolated = df_missing.interpolate()
+    print("Original data shape:", df_missing.shape)
+    print("Data after dropping missing values:", df_dropped.shape)
+    print("Data after interpolation:")
+    print(df_interpolated.head(60))
+
+handling_missing_values
+
+# Filtering EEG Signals
+
+def filtering_signals():
+
+    start_time = '2024-01-11 00:00:00.200'
+    end_time = '2024-01-11 00:00:00.400'
+    df_time_filtered = df.loc[start_time:end_time]
+
+    #filtering the EEG signals keeping Fp1 and F3
+    channels_to_keep = ['Fp1', 'F3']
+    df_channel_filtered = df_time_filtered[channels_to_keep]
+
+    # Condition applied (Amplitude 1.5)
+    df_condition_filtered = df_channel_filtered[df_channel_filtered['Fp1'] > 1.5]
+
+    print("Filtered Data:")
+    print(df_condition_filtered)
+filtering_signals
